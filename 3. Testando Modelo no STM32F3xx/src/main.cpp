7b444397505c568cd9118bd18f9ca85903e39423
Maurício Taffarel <mauruciotaffarel@gmail.com>
2023-08-17 15:26:14 -0300

    🔥 remove deps

--- 3. Testando Modelo no STM32F3xx/src/main.cpp
+++ 3. Testando Modelo no STM32F3xx/src/main.cpp
@@ -1,17 +1,7 @@
 #include "led.h"
 #include "stm32_app.h"
-#include "EloquentTinyML.h"
 #include "sine_model.h"
 #include <stdio.h>
-
-#define NUMBER_OF_INPUTS 1
-#define NUMBER_OF_OUTPUTS 1
-#define TENSOR_ARENA_SIZE 3168
-
-Eloquent::TinyML::TfLite<
-    NUMBER_OF_INPUTS,
-    NUMBER_OF_OUTPUTS,
-    TENSOR_ARENA_SIZE> ml;
 
 UART_HandleTypeDef huart1;
 
@@ -24,19 +14,9 @@ void setup(void) {
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
-
-  ml.begin(sine_model);
 }
 
 void loop(void) {
   HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-  //printf("Hello, world!\n");
-  
-  for (int i = 0; i < 3.14; i=i+0.001) {
-    float x = 3.14 * i / 100.0;
-    float input[1] = { x };
-    float predicted = ml.predict(input);
-  }
-  
-  delay(100);
+  delay(1000);
 }
